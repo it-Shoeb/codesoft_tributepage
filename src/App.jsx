425d40e46b29d36a80985653a6a050c80411f542
it-Shoeb <sshoeb577@gmail.com>
2024-09-01 10:48:03 +0530

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="wrapper">
+      <aside>
+        <div className="suggestion">
+          <a href="">APJ Abdul Kalam</a>
+          <a href="">APJ Abdul Kalam</a>
+          <a href="">APJ Abdul Kalam</a>
+          <a href="">APJ Abdul Kalam</a>
+          <a href="">APJ Abdul Kalam</a>
+        </div>
+        <hr />
+        <div className="image-container">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Muhammad_Ali_NYWTS.jpg/330px-Muhammad_Ali_NYWTS.jpg" alt="" />
+        </div>
+          <h1 className='name'>Muhammad Ali</h1>
+        <hr />
+        <div className="Information">
+          <h3>Information</h3>
+          <ul>
+            <li><strong>Born:  </strong>Cassius Marcellus Clay Jr.January 17, 1942 Louisville, Kentucky, U.S.</li>
+            <li><strong>Died:  </strong>June 3, 2016 (aged 74) Scottsdale, Arizona, U.S.</li>
+            <li><strong>Muhammad Ali:  </strong> (/ɑːˈliː/;[2] born Cassius Marcellus Clay Jr.; January 17, 1942 – June 3, 2016) was an American professional boxer and activist. Nicknamed "the Greatest"</li>
+            <li><strong></strong></li>
+          </ul>
+        </div>
+        <hr />
+        <div className="resources">
+          <h3>Resources</h3>
+          <div className="social-icons">
+              <a href='https://en.wikipedia.org/wiki/Muhammad_Ali' target='_blank'><img src="https://en.wikipedia.org/static/images/mobile/copyright/wikipedia-wordmark-en.svg" alt="" /></a>
+          </div>
+        </div>
+      </aside>
+      <main>
+        <ul>
+          <li>Born and raised in Louisville, Kentucky, he began training as an amateur boxer at age 12. At 18, he won a gold medal in the light heavyweight division at the 1960 Summer Olympics and turned professional later that year.</li>
+          <li>He converted to Islam after 1961. He won the world heavyweight championship, defeating Sonny Liston in a major upset on February 25, 1964, at age 22. During that year, he denounced his birth name as a "slave name" and formally changed his name to Muhammad Ali</li>
+          <li>He fought in several historic boxing matches, including his highly publicized fights with Sonny Liston, Joe Frazier (including the Fight of the Century, the biggest boxing event up until then), the Thrilla in Manila, and his fight with George Foreman in The Rumble in the Jungle.</li>
+          <li>Clay was first directed toward boxing by Louisville police officer and boxing coach Joe E. Martin,[19] who encountered the 12-year-old fuming over a thief having taken his bicycle. He told the officer he was going to "whup" the thief. The officer told Clay he had better learn how to box first.</li>
+          <li>Clay made his professional debut on October 29, 1960, winning a six-round decision over Tunney Hunsaker. From then until the end of 1963, Clay amassed a record of 19–0 with 15 wins by knockout. He defeated boxers including Tony Esperti, Jim Robinson, Donnie Fleeman, Alonzo Johnson, George Logan, Willi Besmanoff, LaMar Clark, Doug Jones, and Henry Cooper. Clay also beat his former trainer and veteran boxer Archie Moore in a 1962 match.</li>
+          <li>Ali defended his title against former heavyweight champion Floyd Patterson on November 22, 1965. Before the match, Ali mocked Patterson, who was widely known to call him by his former name Cassius Clay, as an "Uncle Tom", calling him "The Rabbit". Although Ali had the better of Patterson, who appeared injured during the fight, the match lasted 12 rounds before being called on a technical knockout.</li>
+          <li>After the Patterson fight, Ali founded his own promotion company, Main Bout. The company mainly handled Ali's boxing promotions and pay-per-view closed-circuit television broadcasts. The company's stockholders were mainly fellow Nation of Islam members, along with several others, including Bob Arum.[45]</li>
+          <li><strong>Impact of Ali's draft refusal </strong> Ali's example inspired many black Americans and others. However, initially when he refused induction, he became arguably the most hated man in the country and received many death threats. People who supported Ali during this time were also threatened, including sports journalist Jerry Izenberg, whose columns defended Ali's decision not to serve. He wrote, "Bomb threats emptied our office, making the staff stand out in the snow. My car windshield was smashed with a sledgehammer."[72][73] The New York Times columnist William Rhoden wrote, "Ali's actions changed my standard of what constituted an athlete's greatness. Possessing a killer jump shot or the ability to stop on a dime was no longer enough. What were you doing for the liberation of your people? What were you doing to help your country live up to the covenant of its founding principles?</li>
+          <li><strong>NSA and FBI monitoring of Ali's communications </strong>In a secret operation code-named "Minaret", the National Security Agency (NSA) intercepted the communications of leading Americans, including Ali, Senators Frank Church and Howard Baker, Dr. Martin Luther King Jr., prominent U.S. journalists, and others who criticized the U.S. war in Vietnam.[80][81] A review by the NSA of the Minaret program concluded that it was "disreputable if not outright illegal".[81]</li>
+          <li><strong>Exile and comeback </strong>In March 1966, Ali refused to be inducted into the armed forces. He was systematically denied a boxing license in every state and stripped of his passport. As a result, he did not fight from March 1967 to October 1970—from ages 25 to almost 29—as his case worked its way through the appeals process before his conviction was overturned in 1971</li>
+          <li><strong>Protesting while exiled </strong>During this time of inactivity, as opposition to the Vietnam War began to grow and Ali's stance gained sympathy, he spoke at colleges across the nation, criticizing the Vietnam War and advocating African American pride and racial justice. Ali based himself in Chicago.[84] According to most close to him, his Chicago years were formative. At the time, Ali was widely condemned by the American media,[85] with fears that his actions could potentially lead to mass civil disobedience.[86] Despite this, Ebony magazine noted in the late 1960s that Ali's popularity had increased during this time, especially among black people.[87]</li>
+          <li><strong>The Super Fight </strong>While banned from sanctioned bouts, Ali settled a $1 million lawsuit against radio producer Murray Woroner by accepting $10,000 to appear in a privately staged fantasy fight against retired champion Rocky Marciano.[88] In 1969 the boxers were filmed sparring for about 75 one-minute rounds; they produced several potential outcomes.[89] A computer program purportedly determined the winner, based on data about the fighters, along with the opinions of approximately 250 boxing experts. Edited versions of the bout were shown in movie theaters in 1970. In the U.S. version Ali lost in a simulated 13th-round knockout, but in the European version Marciano lost due to cuts, also simulated.[90] Ali suggested that prejudice determined his defeat in the U.S. version; he was reported to have jokingly said, "That computer was made in Alabama."[88]</li>
+          <li><strong>Return </strong>On August 11, 1970, with his case still in appeal, Ali was granted a license to box by the City of Atlanta Athletic Commission. Leroy Johnson, Jesse Hill Jr. and Harry Pett had used their local political influence and set up the company House of Sports to organize the fight, underlining the influential power of Georgia's black politics in Ali's comeback.[91] Ali's first return bout was against Jerry Quarry on October 26, resulting in a win after three rounds after Quarry was cut.</li>
+          <li><strong>After his loss </strong>In 1971, basketball star Wilt Chamberlain challenged Ali to a fight, and a bout was scheduled for July 26. Although the seven-foot-two-inch-tall Chamberlain had formidable physical advantages over Ali—weighing 60 pounds more and able to reach 14 inches further—Ali was able to influence Chamberlain into calling off the bout by taunting him with calls of "Timber!" and "The tree will fall" during a shared interview. These statements of confidence unsettled his taller opponent, whom Los Angeles Lakers owner Jack Kent Cooke had offered a record-setting contract, conditional on Chamberlain agreeing to abandon what Cooke termed "this boxing foolishness",[96] and he did exactly that.[97] To replace Ali's opponent, promoter Bob Arum quickly booked a former sparring partner of Ali's, Jimmy Ellis, who was a childhood friend from Louisville, Kentucky, to fight him. Ali won the bout through a technical knockout when the referee stopped the fight in the twelfth round.[98]</li>
+          <li><strong>Fights against Quarry, Patterson, Foster, Bugner and Norton </strong>After the loss to Frazier, Ali fought Jerry Quarry, had a second bout with Floyd Patterson and faced Bob Foster in 1972, winning a total of six fights that year. During two bouts he had in 1973 with Joe Bugner and Ken Norton, he wore a "People's Choice" robe which was given to him by Elvis Presley.[99] In 1973 before his fight with Norton, Tom Cushman said Ali was "gloriously overconfident. He didn't consider this guy a threat at all."[100] But during the fight, either in the 2nd round according to most press reports, or the final round according to Norton,[100] Norton broke Ali's jaw and inflicted by decision the second loss of his career. After considering retirement, Ali won a controversial decision against Norton in their second bout. This led to a rematch with Joe Frazier at Madison Square Garden on January 28, 1974; Frazier had recently lost his title to George Foreman.</li>
+        </ul>
+      </main>
+    </div>
+    </>
+  )
+}
+
+export default App
